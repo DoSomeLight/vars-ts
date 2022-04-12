df6e075f1581160e0955cb7556e08f46bb4438c3
Oleg Chepelev <yorkblansh@gmail.com>
2022-04-12 22:34:01 +0300

    ✨ feat(cron-task-manager): update types

---
+++ cron-task-manager.ts
@@ -0,0 +1,16 @@
+export interface TaskPropsInterface {
+	job_key: string
+	cron_exp?: string
+	task?: string
+}
+
+export enum TaskEventsEnum {
+	ADD_TASK = 'ADD_TASK',
+	START_TASK = 'START_TASK',
+	STOP_TASK = 'STOP_TASK',
+	UPDATE_TASK = 'UPDATE_TASK',
+}
+
+export type TaskManager = {
+	[key in TaskEventsEnum]: (taskProps: TaskPropsInterface) => void
+}
