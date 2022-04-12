de19b0fbd55b1882e4ad15d1d2a2a1e88118751f
Oleg Chepelev <yorkblansh@gmail.com>
2022-04-12 22:47:47 +0300

    ✨ feat(returnLiteralDeviceTypefromNum): update

--- fns.ts
+++ fns.ts
@@ -5,16 +5,16 @@ import _ from 'lodash'
 export const ConvertEnum = <K extends string, V extends string | number>(_enum: EnumType<K, V>) => ({
 	toObjectArray: () =>
 		Object.entries<V>(_enum).map(([key, value]) => ({
-			key,
-			value,
+			['key']: key,
+			['value']: value,
 		})),
 })
 
-// export const returnLiteralDeviceTypefromNum = (device_type: number) =>
-// 	_.compact(
-// 		all_device_types.map(
-// 			({ key, value }: { key: keyof typeof DEVICE_TYPES; value: DEVICE_TYPES }) => device_type === value && key,
-// 		),
-// 	)[0]
-
-type Tenum<K extends string, V extends string | number> = { [s in K]: V }
+export const returnLiteralDeviceTypefromNum = (device_type: number) =>
+	_.compact(
+		ConvertEnum(DEVICE_TYPES)
+			.toObjectArray()
+			.map(
+				({ key, value }: { key: keyof typeof DEVICE_TYPES; value: DEVICE_TYPES }) => device_type === value && key,
+			),
+	)[0]
