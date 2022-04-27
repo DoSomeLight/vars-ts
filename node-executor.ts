d738331385293881fa5c897832372011780f8544
Oleg Chepelev <yorkblansh@gmail.com>
2022-04-27 18:43:04 +0300

    ✨ feat(node-executor): add ExecutorEventsEnum

--- node-executor.ts
+++ node-executor.ts
@@ -1,5 +1,11 @@
 import { DEVICE_TYPES, DeviceToggleState } from '.'
 
+export enum ExecutorEventsEnum {
+	toggle_device = 'toggle_device',
+	set_limit_up = 'set_limit_up',
+	set_limit_down = 'set_limit_down',
+}
+
 export interface ActionFeat {
 	toggle?: keyof typeof DEVICE_TOOGLE_STATUS
 	temp_limit?: { up?: string; down?: string }
